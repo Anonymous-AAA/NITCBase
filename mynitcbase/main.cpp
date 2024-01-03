1c29bbda0ef327675ca5950d3e1f265abdccde50
Anonymous-AAA <alenantonyme@gmail.com>
2024-01-03 15:38:29 +0530

    Stage 2 basic

--- mynitcbase/main.cpp
+++ mynitcbase/main.cpp
@@ -2,44 +2,54 @@
 #include "Cache/OpenRelTable.h"
 #include "Disk_Class/Disk.h"
 #include "FrontendInterface/FrontendInterface.h"
+#include "define/constants.h"
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-  /* Initialize the Run Copy of Disk */
+
+  // creating run copy of the disk
   Disk disk_run;
-  // StaticBuffer buffer;
-  // OpenRelTable cache;
-  
-  unsigned char buffer[BLOCK_SIZE];
-  //BLOCK_SIZE is a constant that has value 2048
-  
-  Disk::readBlock(buffer, 7000);
-  // 7000 is a random block number that's unused.
-  
-  char message[] = "hello";
-  memcpy(buffer + 20, message, 6);
-  //Now, buffer[20] = 'h', buffer[21] = 'e' ...
-  Disk::writeBlock(buffer, 7000);
 
-  unsigned char buffer2[BLOCK_SIZE];
-  char message2[6];
-  Disk::readBlock(buffer2, 7000);
-  memcpy(message2, buffer2 + 20, 6);
-  std::cout << message2<<'\n';
+  // create objects for the relation catalog and attribute catalog
+  RecBuffer relCatBuffer(RELCAT_BLOCK);
+  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
 
+  HeadInfo relCatHeader;
+  HeadInfo attrCatHeader;
 
-  unsigned char buffer3[BLOCK_SIZE];
+  // load the headers of both the blocks into relCatHeader and attrCatHeader.
+  // (we will implement these functions later)
+  relCatBuffer.getHeader(&relCatHeader);
+  attrCatBuffer.getHeader(&attrCatHeader);
 
-  for(int i=0;i<4;i++)  {
-    Disk::readBlock(buffer3,i);
+  for (int i = 0; i < relCatHeader.numEntries; i++) {
 
-    for(int j=0;j<BLOCK_SIZE;j++)
-      std::cout << (int) buffer3[j]<<' ';
+    // attribute array to store a record
+    Attribute relCatRecord[RELCAT_NO_ATTRS]; // will store the record from the
+                                             // relation catalog
 
+    relCatBuffer.getRecord(relCatRecord, i);
+
+    printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+
+    for (int j = 0; j < attrCatHeader.numEntries; j++) {
+
+      // declare attrCatRecord and load the attribute catalog entry into it
+      Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+
+      attrCatBuffer.getRecord(attrCatRecord, j);
+
+      if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal,
+                 relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0) {
+        const char *attrType =
+            attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM"
+                                                                  : "STR";
+        printf("  %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal,
+               attrType);
+      }
+    }
+    printf("\n");
   }
 
-  std::cout<<'\n';
-
   return 0;
-  //return FrontendInterface::handleFrontend(argc, argv);
 }
---
+++ notes/stage2.md
@@ -0,0 +1,16 @@
+# Internal Index Block Structure
+
+- Each block stores a maximum of 100 attribute values
+- Each of these values has an associated pair of left and right child pointers
+- There will be 101 child pointers
+- First 32 bytes store the header followed by actual attribute key values and child pointers
+- Header similar to that of a record block.
+- Child pointer is of 4 bytes and attribute key of 16 bytes
+- 12 bytes remain unused
+
+# REFER DOC for rest
+
+# Important points
+
+- Each slot stores a single record of a relation.
+- Maximum number of relations allowed in NITCBase is 18
